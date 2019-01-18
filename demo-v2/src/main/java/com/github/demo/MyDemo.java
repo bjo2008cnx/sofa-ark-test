ba0511beda92697ddbcb33a8f85f43c399af9171
michael <michael@weimob.com>
2019-01-18 16:55:26 +0800

    add service v1&v2

--- demo-v2/src/main/java/com/github/demo/MyDemo.java
+++ demo-v2/src/main/java/com/github/demo/MyDemo.java
@@ -2,6 +2,6 @@ package com.github.demo;
 
 public class MyDemo {
     public static void execute() {
-        System.out.println(MyDemo.class.getClassLoader());
+        System.out.println("v2: " + MyDemo.class.getClassLoader());
     }
 }
---
+++ service-v1/src/main/java/com/github/demo/MyServiceV1.java
@@ -0,0 +1,7 @@
+package com.github.demo;
+
+public class MyServiceV1 {
+    public static void execute() {
+        MyDemo.execute();
+    }
+}
